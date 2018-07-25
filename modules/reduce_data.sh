c97281578b0d828d3c2e9e2c20c560a31b96329f
matthewclarkmay <mcmay.web@gmail.com>
2018-07-25 03:27:47 +0000

    tweaked license

--- modules/reduce_data.sh
+++ modules/reduce_data.sh
@@ -3,11 +3,9 @@
 # Author:
 # Matt May <mcmay.web@gmail.com>
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License.  You may obtain a
-# copy of
-# the License at
+# copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
